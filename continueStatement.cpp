eee8fe7c9c2e021da871cc190d040b64541d393b
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2022-08-21 10:35:47 +0530

    first commit

---
+++ continueStatement.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int  i = 1; i <= 10; i++)
+    {
+       if(i == 7){
+        continue;
+       }
+       else
+        cout << i << endl;
+    }
+    
+}
